452581e6107c2c1a80d68950471264da8ac81965
yuanbin2 <109586436+yuanbin2@users.noreply.github.com>
2024-02-14 12:30:26 +0800

    change key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -9,7 +9,7 @@ export interface FetchResponse<T> {
 const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: 'e53bba19d7914970889f528d70c8e06d',
+    key: 'def7d2e15a7e49c691b5ec85798164cc',
   },
 });
 
